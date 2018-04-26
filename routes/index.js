fc365c26282062ea9f045d8db74ee98b0eb2bbf1
Yogesh Kale <yogeshkale@Yogeshs-MacBook-Air.local>
2018-04-26 18:19:48 +0530

    Project ready without promise, model and helpers

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,14 @@
 var express = require('express');
+var random = require('random-number');
+var dateTime = require('node-datetime');
 var db_conection = require('./connection.js');
 var router = express.Router();
+
+var gen = random.generator({
+	min:  0,
+	max:  1000,
+	integer: true
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,17 +20,15 @@ router.get('/', function(req, res, next) {
 	}
 	else
 	{
-		res.render('index', { title: 'Login', success: req.session.success, errors: req.session.errors});	
+		res.render('index', { title: 'Login', success: req.session.success, errors: req.session.errors});
+		delete req.session.errors;
 	}	
 });
 
 router.post('/login_action', function(req, res, next){
 
-	// console.log(req.body);
-	// req.checkBody('fname', 'first name is required').notEmpty();
-	// req.checkBody('lname', 'Last name is required').notEmpty();
 	req.checkBody('email_id', 'Email id is required').notEmpty();
-	// req.checkBody('mobile_no', 'Mobile No is required').notEmpty();
+	req.checkBody('email_id', 'A valid email is required').isEmail();
 	req.checkBody('password', 'Password is required').notEmpty();
 
 	var errors = req.validationErrors();
@@ -32,8 +38,7 @@ router.post('/login_action', function(req, res, next){
 		req.session.errors = errors;
       	req.session.success = false;
       	res.redirect('/');
-	}
-	else {
+	} else {
 		db_conection.select('users', 'id, CONCAT(fname, " ", lname) as fullname', '`email_id`="'+req.body.email_id+'" AND `password`="'+req.body.password+'"', function(err, data){
 
 			if(err){
@@ -44,7 +49,6 @@ router.post('/login_action', function(req, res, next){
 
 					req.session.user_id = data[0]['id'];
 					req.session.username = data[0]['fullname'];
-					// res.render('my_profile', { user_id : req.session.user_id, username: req.session.username});
 					res.redirect('/profile');
 				} else {
 					res.redirect('/');
@@ -62,59 +66,74 @@ router.get('/register', function(req, res, next) {
 	}
 	else
 	{
-		res.render('register_form', { title: 'User Register' });
+		res.render('register_form', { title: 'User Register', success: req.session.success, errors: req.session.errors});
+		delete req.session.errors;
 	}
 });
 
 router.post('/register_action', function(req, res, next){
 
-	// console.log(db_conection);
-	console.log(req.body);
-	console.log(req.files);
+	req.checkBody('fname', 'first name is required').notEmpty();
+	req.checkBody('lname', 'Last name is required').notEmpty();
+	req.checkBody('email_id', 'Email id is required').notEmpty();
+	req.checkBody('email_id', 'A valid email is required').isEmail();
+	req.checkBody('mobile_no', 'Mobile No is required').notEmpty();
+	req.checkBody('password', 'Password is required').notEmpty();
+
 	var file_name = req.files.filetoupload;
 
-	// console.log(req.body);
-	// console.log(req.files);
-	db_conection.insert('users', ['fname','lname','email_id','mobile_no','password'], [req.body.fname, req.body.lname, req.body.email_id, req.body.mobile_no, req.body.password], function(err, data){
+	var errors = req.validationErrors();
+	
+	if(errors){
 
-		if(err){
-			console.log(err);
-		} else {
+		req.session.errors = errors;
+      	req.session.success = false;
+      	res.redirect('/register');
 
-			var last_user_id = data.insertId;
+	} else {
 
-			file_name.mv("public/uploads/"+ file_name.name, function(err){
+		db_conection.insert('users', ['fname','lname','email_id','mobile_no','password'], [req.body.fname, req.body.lname, req.body.email_id, req.body.mobile_no, req.body.password], function(err, data){
 
-				if(err){
-					console.log(err);
-				} else {
-					
-					db_conection.update('users', ['profile_pic'], [file_name.name], 'id='+last_user_id, function(err2, data2){
+			if(err){
+				console.log(err);
+			} else {
 
-						if(err) {
-							console.log(err2);
-						} else {
-							console.log(data2);
-						}
-					});
-				}
-			})
+				var last_user_id = data.insertId;
 
-			res.redirect('/');
-		}
-	});
+				file_name.mv("public/uploads/"+ file_name.name, function(err){
+
+					if(err){
+						console.log(err);
+					} else {
+						
+						db_conection.update('users', ['profile_pic'], [file_name.name], 'id='+last_user_id, function(err2, data2){
+
+							if(err) {
+								console.log(err2);
+							} else {
+								console.log(data2);
+							}
+						});
+					}
+				})
+
+				res.redirect('/');
+			}
+		});
+	}
 });
 
 router.get('/profile', function(req, res, next) {
-	console.log(req.session);
+	// console.log(req.session);
 	if(req.session.user_id)
 	{
 		db_conection.select('users', 'id, fname, lname, email_id, mobile_no, profile_pic, password', 'id='+req.session.user_id, function(err, data){
-			console.log(data);
+
 			if(err){
 				console.log(err)
 			} else {
-				res.render('my_profile', { title: 'My Profile', user_id : req.session.user_id, username: req.session.username, fname:data[0]['fname'], lname:data[0]['lname'], email_id:data[0]['email_id'], mobile_no:data[0]['mobile_no'], profile_pic:data[0]['profile_pic'], password:data[0]['password']});
+				res.render('my_profile', { title: 'My Profile', user_id : req.session.user_id, username: req.session.username, fname:data[0]['fname'], lname:data[0]['lname'], email_id:data[0]['email_id'], mobile_no:data[0]['mobile_no'], profile_pic:data[0]['profile_pic'], password:data[0]['password'], success: req.session.success, errors: req.session.errors});
+				delete req.session.errors;
 			}
 		})	
 	}
@@ -125,35 +144,68 @@ router.get('/profile', function(req, res, next) {
 });
 
 router.post('/update_profile_action', function(req, res, next){
-	
-	var file_name = req.files.filetoupload;
-	console.log(req.files);
 
-	if(req.body.user_id) {
+	//generate random number and datetime stamp for image name
+	var random_number = gen();
+	var dt = dateTime.create();
+	var formatted = dt.format('Y-m-d-H:M:S');
 
-		if(req.files){
+	//set validation rules
+	req.checkBody('fname', 'first name is required').notEmpty();
+	req.checkBody('lname', 'Last name is required').notEmpty();
+	req.checkBody('email_id', 'Email id is required').notEmpty();
+	req.checkBody('email_id', 'A valid email is required').isEmail();
+	req.checkBody('mobile_no', 'Mobile No is required').notEmpty();
+	req.checkBody('password', 'Password is required').notEmpty();
 
-			file_name.mv("public/uploads/"+ file_name.name, function(err){
-				if(err){
-					console.log(err);
-					// var profile_pic_name = '';
-				} else {
-					// var profile_pic_name = file_name.name;
-				}
-			});
-		}
+	var errors = req.validationErrors()
 
-		db_conection.update('users', ['fname','lname','email_id','mobile_no','password', 'profile_pic'], [req.body.fname, req.body.lname, req.body.email_id, req.body.mobile_no, req.body.password, file_name.name], 'id='+req.body.user_id, function(err, data){
+	if(errors){
 
-			if(err){
-				console.log(err);
-			} else {
-				res.redirect('/profile');
-			}
-		});
+		req.session.errors = errors;
+      	req.session.success = false;
+      	res.redirect('/profile');
 
 	} else {
-		redirect('/profile');
+
+		if(req.body.user_id) {
+			var profile_pic_name ;
+
+			if(req.files.filetoupload){
+
+				var file_name = req.files.filetoupload;
+				profile_pic_name = formatted+'-'+random_number+'.jpg';
+				// var previos_name_array = file_name.name.split('.');
+
+				file_name.mv("public/uploads/"+ profile_pic_name, function(err){
+					
+					if(err){
+						console.log(err);
+						profile_pic_name = '';
+					}
+				});
+
+			} else {
+
+				if(req.body.previous_image){
+					profile_pic_name = req.body.previous_image;
+				} else {
+					profile_pic_name = '';
+				}
+			}
+
+			db_conection.update('users', ['fname','lname','email_id','mobile_no','password', 'profile_pic'], [req.body.fname, req.body.lname, req.body.email_id, req.body.mobile_no, req.body.password, profile_pic_name], 'id='+req.body.user_id, function(err, data){
+
+				if(err){
+					console.log(err);
+				} else {
+					res.redirect('/profile');
+				}
+			});
+
+		} else {
+			redirect('/profile');
+		}
 	}
 });
 
